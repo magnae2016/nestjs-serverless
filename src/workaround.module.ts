8d8b317a9bdaecefd36389ceaf52c0d0e9472478
김현이/선임연구원/ID클라우드솔루션Project <hyunyi.kim@lge.com>
2023-12-27 20:36:25 +0900

    Add middleware forRoutes('users')

--- src/workaround.module.ts
+++ src/workaround.module.ts
@@ -1,9 +1,16 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { UsersController } from "./users/users.controller";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
 
 @Module({
   imports: [],
   controllers: [UsersController],
   providers: [],
 })
-export class WorkaroundModule {}
+export class WorkaroundModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('users');
+  }
+}
